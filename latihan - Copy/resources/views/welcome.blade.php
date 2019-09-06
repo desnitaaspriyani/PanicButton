ff2ae7b80b77f14ac96b227fd91471c50096fb5c
Riko Raynol Hasan <rikoraynol12345@gmail.com>
2019-09-06 13:08:54 +0800

    baru2

--- latihan - Copy/resources/views/welcome.blade.php
+++ latihan - Copy/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    KELAS TRPL B
                 </div>
 
                 <div class="links">
--- latihan/resources/views/mahasiswa/edit.blade.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head></head>
-<body>
-  <form method="post" action="{!!url('mhs/'.$mhs->id)!!}">
-    <input type="hidden" name="_method" value="PUT">
-    @include('mahasiswa.fields')
-  </form>
-</body>
-</html>
